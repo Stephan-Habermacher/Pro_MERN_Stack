18d070f9caab78208e2325311b972ff4119d3041
Stephan <stephan_habermacher@gmx.ch>
2024-01-20 16:48:35 +0100

    test-chapter-3

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+class HelloWorld extends React.Component {}
+
 const continents = ["Africa", "America", "Asia", "Australia", "Europe"];
 const helloContinents = Array.from(continents, (c) => `Hello ${c}!`);
 const message = helloContinents.join(" ");
