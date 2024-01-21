9df2e58f28295374750639d1ac714ef77222fcb9
Stephan <stephan_habermacher@gmx.ch>
2024-01-21 12:02:46 +0100

    Endpoint 3.2

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,36 @@
-class HelloWorld extends React.Component {}
+class IssueFilter extends React.Component {
+  render() {
+    return <div>This is a placeholder for the issue filter.</div>;
+  }
+}
 
-const continents = ["Africa", "America", "Asia", "Australia", "Europe"];
-const helloContinents = Array.from(continents, (c) => `Hello ${c}!`);
-const message = helloContinents.join(" ");
+class IssueTable extends React.Component {
+  render() {
+    return <div>This is a placeholder for a table of issues.</div>;
+  }
+}
 
-const element = (
-  <div title="Outer div">
-    <h1>{message}</h1>
-  </div>
-);
+class IssueAdd extends React.Component {
+  render() {
+    return <div>This is a placeholder for a form to add an issue.</div>;
+  }
+}
+
+class IssueList extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <h1>Issue Tracker</h1>
+        <IssueFilter />
+        <hr />
+        <IssueTable />
+        <hr />
+        <IssueAdd />
+      </React.Fragment>
+    );
+  }
+}
+
+const element = <IssueList />;
+
 ReactDOM.render(element, document.getElementById("contents"));
